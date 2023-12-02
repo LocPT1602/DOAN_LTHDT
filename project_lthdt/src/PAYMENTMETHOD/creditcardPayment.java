3c256291986c51f0a42be4409d82a4095d2247f8
KhanhBtt1305 <152600240+KhanhBtt1305@users.noreply.github.com>
2023-12-02 22:58:49 +0700

    credit

--- project_lthdt/src/PAYMENTMETHOD/creditcardPayment.java
+++ project_lthdt/src/PAYMENTMETHOD/creditcardPayment.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class creditcardPayment extends payment {
 	private String sothe, tenthe;
+	private String matkhau, tenchuthe;
 	Scanner sc = new Scanner(System.in);
 
 	public String getSothe() {
@@ -21,20 +22,42 @@ public class creditcardPayment extends payment {
 	public void setTenthe(String tenthe) {
 		this.tenthe = tenthe;
 	}
+	public String getMatkhau() {
+		return matkhau;
+	}
+
+	public void setMatkhau(String matkhau) {
+		this.matkhau = matkhau;
+	}
+
+	public String getTenchuthe() {
+		return tenchuthe;
+	}
+
+	public void setTenchuthe(String tenchuthe) {
+		this.tenchuthe = tenchuthe;
+	}
 
 	@Override
 	public void Nhap() {
 		super.Nhap();
 		System.out.println("Nhap thong tin the: ");
+		System.out.println("Nhap so the: ");
+		this.sothe = sc.nextLine();
+		System.out.println("Nhap ten the: ");
+		this.tenthe = sc.nextLine();
 		System.out.println("Nhap mat khau: ");
-		String matkhau = sc.nextLine();
+		this.matkhau = sc.nextLine();
 		System.out.println("Nhap ten chu the: ");
-		String tenchuthe = sc.nextLine();
+		this.tenchuthe = sc.nextLine();
 	}
 
 	@Override
 	public String toString() {
-		return "sothe=" + sothe +
-				"\ntenthe=" + tenthe;
+		setTrangthai("Completed");
+		return "So the: " + sothe + 
+				"\nTen the: " + tenthe + 
+				"\nTen chu the: " + tenchuthe +
+				"\nTrangthai: " + getTrangthai() ;
 	}
 }
