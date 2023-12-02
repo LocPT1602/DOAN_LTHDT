51cfdd466c2fae04b63001c07fbcd570c6577a2b
AnhTuan054 <149463397+AnhTuan054@users.noreply.github.com>
2023-12-02 18:11:39 +0700

    .

--- project_lthdt/src/ORDER/instore.java
+++ project_lthdt/src/ORDER/instore.java
@@ -8,8 +8,8 @@ import PRODUCTS.SanPham;
 public class instore extends order {
     private double tienmat;
 Scanner sc = new Scanner(System.in);
-    public instore(String orderCode, String customerCode, String employeeCode, String orderDate, List<SanPham> SanPhamList, int quantity, double totalValue, boolean paymentConfirmed, boolean orderConfirmed, double tienmat) {
-        super(orderCode, customerCode, employeeCode, orderDate, SanPhamList, quantity, totalValue, paymentConfirmed, orderConfirmed);
+    public instore(String orderCode, String customerCode, String employeeCode, String orderDate, List<SanPham> SanPhamList, int quantity, double totalValue, boolean paymentConfirmed, boolean orderConfirmed, double tienmat, String status) {
+        super(orderCode, customerCode, employeeCode, orderDate, SanPhamList, quantity, totalValue, paymentConfirmed, orderConfirmed, status);
         this.tienmat = tienmat;
     }
     public instore(){
