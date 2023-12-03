4ebb11261a197f87b756d317af5e894838854a12
AnhTuan054 <149463397+AnhTuan054@users.noreply.github.com>
2023-12-03 17:42:22 +0700

    .

--- project_lthdt/src/ORDER/BillDetail.java
+++ project_lthdt/src/ORDER/BillDetail.java
@@ -3,7 +3,6 @@ package ORDER;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Scanner;
 
 public class BillDetail {
     private int billDetailCode; // Mã chi tiết hóa đơn
@@ -13,19 +12,12 @@ public class BillDetail {
     private String paymentMethod; // Phương thức thanh toán
     private int quantity; // Số lượng
     private LocalDate billDate; // Ngày trên hóa đơn
-    Scanner sc = new Scanner(System.in);
 
+    // Constructors
     public BillDetail() {
-        this.billDetailCode = 0;
-        this.customerCode = 0;
-        this.employeeCode = 0;
-        this.paymentMethod = " ";
-        this.quantity = 0;
-        this.billDate = null;
     }
 
-    public BillDetail(int billDetailCode, int customerCode, int employeeCode, bill bill, String paymentMethod,
-            int quantity, LocalDate billDate) {
+    public BillDetail(int billDetailCode, int customerCode, int employeeCode, bill bill, String paymentMethod, int quantity, LocalDate billDate) {
         this.billDetailCode = billDetailCode;
         this.customerCode = customerCode;
         this.employeeCode = employeeCode;
@@ -35,13 +27,13 @@ public class BillDetail {
         this.billDate = billDate;
     }
 
-    // Get và Set
+    // Getters and Setters
     public int getBillDetailCode() {
         return billDetailCode;
     }
 
     public void setBillDetailCode(int billDetailCode) {
-        billDetailCode = sc.nextInt();
+        this.billDetailCode = billDetailCode;
     }
 
     public int getCustomerCode() {
@@ -49,7 +41,7 @@ public class BillDetail {
     }
 
     public void setCustomerCode(int customerCode) {
-        this.customerCode = sc.nextInt();
+        this.customerCode = customerCode;
     }
 
     public int getEmployeeCode() {
@@ -57,7 +49,7 @@ public class BillDetail {
     }
 
     public void setEmployeeCode(int employeeCode) {
-        this.employeeCode = sc.nextInt();
+        this.employeeCode = employeeCode;
     }
 
     public bill getBill() {
@@ -73,7 +65,7 @@ public class BillDetail {
     }
 
     public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = sc.nextLine();
+        this.paymentMethod = paymentMethod;
     }
 
     public int getQuantity() {
@@ -81,7 +73,7 @@ public class BillDetail {
     }
 
     public void setQuantity(int quantity) {
-        this.quantity = sc.nextInt();
+        this.quantity = quantity;
     }
 
     public LocalDate getBillDate() {
@@ -89,7 +81,7 @@ public class BillDetail {
     }
 
     public void setBillDate(LocalDate billDate) {
-        this.billDate = LocalDate.now();
+        this.billDate = billDate;
     }
 
     // Phương thức tính tổng giá trị hóa đơn
@@ -109,15 +101,13 @@ public class BillDetail {
                 "Số lượng: " + quantity + "\n" +
                 "Ngày hóa đơn: " + billDate + "\n" +
                 "Tổng giá trị: " + calculateTotalAmount();
+        System.out.println(billDetail);
         return billDetail;
     }
-
     // Phương thức ghi thông tin hóa đơn vào tập tin văn bản
-    public void writeToFile(String fileName, String billDetail) {
-        try {
-            FileWriter writer = new FileWriter(fileName);
-            writer.write(billDetail);
-            writer.close();
+    public void writeToFile(String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(getBillDetail());
             System.out.println("Thông tin hóa đơn đã được ghi vào tập tin: " + fileName);
         } catch (IOException e) {
             System.out.println("Đã xảy ra lỗi khi ghi vào tập tin: " + fileName);
@@ -125,8 +115,32 @@ public class BillDetail {
         }
     }
 
+   // Phương thức lấy thông tin đơn hàng và gán vào mảng bill
+public void getInfo(bill[] bills) {
+    // Lấy thông tin đơn hàng từ nguồn dữ liệu hoặc các tham số truyền vào
+    // Ví dụ: giả sử có danh sách mã hóa đơn và giá sản phẩm
+    
+    int[] billCodes = {1, 2, 3};
+    double[] productPrices = {10.0, 20.0, 30.0};
+    
+    // Kiểm tra số lượng đơn hàng và mảng bills có cùng kích thước hay không
+    if (billCodes.length == productPrices.length && billCodes.length == bills.length) {
+        // Lấy thông tin từng đơn hàng và gán vào mảng bills
+        for (int i = 0; i < bills.length; i++) {
+            bill bill = new bill();
+            bill.setBillCode("BILL-" + billCodes[i]);
+            bill.setProductPrice(productPrices[i]);
+            
+            bills[i] = bill;
+        }
+    } else {
+        System.out.println("Số lượng đơn hàng không khớp với kích thước của mảng bills.");
+    }
+}
+
     public static void main(String[] args) {
         BillDetail billDetail = new BillDetail();
+        String fileName = "project_lthdt\\src\\ORDER\\billdetail.txt";
         billDetail.setBillDetailCode(1);
         billDetail.setCustomerCode(123);
         billDetail.setEmployeeCode(456);
@@ -135,12 +149,12 @@ public class BillDetail {
         bill bill = new bill();
         bill.setBillCode("ABC123");
         bill.setProductPrice(10.0);
-        String fileName = "billdetail.txt";
+
         billDetail.setBill(bill);
         billDetail.setPaymentMethod("Credit Card");
-        billDetail.setQuantity(5);
+        billDetail.setQuantity(90);
         billDetail.setBillDate(LocalDate.now());
-
-        billDetail.writeToFile("billdetail.txt", billDetail.toString());
+        billDetail.getBillDetail();
+        billDetail.writeToFile(fileName);
     }
 }
