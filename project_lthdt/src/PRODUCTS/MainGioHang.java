d93ba92c613a446f84d257d67a720b0969e53c33
SkinnySnail <dunguyendangkhoa12a8@gmail.com>
2023-12-10 23:06:01 +0700

    check ma sp trung lap

--- project_lthdt/src/PRODUCTS/MainGioHang.java
+++ project_lthdt/src/PRODUCTS/MainGioHang.java
@@ -13,7 +13,7 @@ public class MainGioHang {
             System.out.println(
                     "1. In danh sach gio hang.\n"
                             + "2. In Gio hang theo ma.\n"
-                            + "3. Them gio hang.\n"
+                            
                             + "4. Lay so gio hang .\n"
                             + "5. Xoa gio hang.\n"
                             + "6. Doc du lieu tu file.\n"
@@ -30,10 +30,7 @@ public class MainGioHang {
                     dsGioHang.layGHtheoMa(sc.nextLine());
                     break;
                 }
-                case 3: {
-
-                    break;
-                }
+               
                 case 4: {
                     dsGioHang.laySize();
                     break;
--- project_lthdt/src/PRODUCTS/Pen.java
+++ project_lthdt/src/PRODUCTS/Pen.java
@@ -20,14 +20,32 @@ public class Pen extends SanPham {
     @Override
     public void tinhNang() {
     }
+    public boolean ktmasp(String maxp) {
+    	kho.docDataTuFile();
+            for (SanPham sp : kho.khoHang) 
+            {
+                if (sp.getMaSP().equals(maxp)) {
+                	return false;
+                }
+            }
+            return true;
+    	}
+    
     @Override
     public void nhap() {
-        System.out.print("\nNhap vao ma san pham: ");
-        this.maSP = kt.kiemtraMaSanpham();
+    	System.out.print("Nhap vao ma san pham: ");
+        String maxp = kt.kiemtraMaSanpham();
+        if(ktmasp(maxp) == true) {
+        	this.maSP = maxp;
+        }
+        else {
+        	System.out.print("Ma san pham nhap sai!!");
+        	this.nhap();
+        }
         System.out.print("\nNhap vao ten san pham: ");
         this.tenSP = sc.nextLine();
         System.out.print("\nNhap vao gia cua san pham: ");
-        this.donGia = kt.KiemTraNhapSoNguyen();
+        this.donGia = Integer.parseInt(sc.nextLine());
     }
 
     @Override
